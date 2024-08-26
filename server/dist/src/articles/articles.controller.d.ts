f535ca8a997f52ad91c7242b89647d30e0681388
webproger1 <Webproger1@gmail.com>
2024-08-26 18:15:14 +0300

    start - CRUD for articles

---
+++ server/dist/src/articles/articles.controller.d.ts
@@ -0,0 +1,45 @@
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+export declare class ArticlesController {
+    private readonly articlesService;
+    constructor(articlesService: ArticlesService);
+    create(createArticleDto: CreateArticleDto): import(".prisma/client").Prisma.Prisma__ArticleClient<{
+        id: number;
+        title: string;
+        description: string | null;
+        body: string;
+        published: boolean;
+        createdAt: Date;
+        updatedAt: Date;
+    }, never, import("@prisma/client/runtime/library").DefaultArgs>;
+    findDrafts(): import(".prisma/client").Prisma.PrismaPromise<{
+        id: number;
+        title: string;
+        description: string | null;
+        body: string;
+        published: boolean;
+        createdAt: Date;
+        updatedAt: Date;
+    }[]>;
+    findAll(): import(".prisma/client").Prisma.PrismaPromise<{
+        id: number;
+        title: string;
+        description: string | null;
+        body: string;
+        published: boolean;
+        createdAt: Date;
+        updatedAt: Date;
+    }[]>;
+    findOne(id: string): import(".prisma/client").Prisma.Prisma__ArticleClient<{
+        id: number;
+        title: string;
+        description: string | null;
+        body: string;
+        published: boolean;
+        createdAt: Date;
+        updatedAt: Date;
+    }, null, import("@prisma/client/runtime/library").DefaultArgs>;
+    update(id: string, updateArticleDto: UpdateArticleDto): string;
+    remove(id: string): string;
+}
---
+++ server/dist/src/articles/articles.controller.js
@@ -0,0 +1,89 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ArticlesController = void 0;
+const common_1 = require("@nestjs/common");
+const articles_service_1 = require("./articles.service");
+const create_article_dto_1 = require("./dto/create-article.dto");
+const update_article_dto_1 = require("./dto/update-article.dto");
+let ArticlesController = class ArticlesController {
+    constructor(articlesService) {
+        this.articlesService = articlesService;
+    }
+    create(createArticleDto) {
+        return this.articlesService.create(createArticleDto);
+    }
+    findDrafts() {
+        return this.articlesService.findDrafts();
+    }
+    findAll() {
+        return this.articlesService.findAll();
+    }
+    findOne(id) {
+        return this.articlesService.findOne(+id);
+    }
+    update(id, updateArticleDto) {
+        return this.articlesService.update(+id, updateArticleDto);
+    }
+    remove(id) {
+        return this.articlesService.remove(+id);
+    }
+};
+exports.ArticlesController = ArticlesController;
+__decorate([
+    (0, common_1.Post)(),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [create_article_dto_1.CreateArticleDto]),
+    __metadata("design:returntype", void 0)
+], ArticlesController.prototype, "create", null);
+__decorate([
+    (0, common_1.Get)('drafts'),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], ArticlesController.prototype, "findDrafts", null);
+__decorate([
+    (0, common_1.Get)(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], ArticlesController.prototype, "findAll", null);
+__decorate([
+    (0, common_1.Get)(':id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], ArticlesController.prototype, "findOne", null);
+__decorate([
+    (0, common_1.Patch)(':id'),
+    __param(0, (0, common_1.Param)('id')),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, update_article_dto_1.UpdateArticleDto]),
+    __metadata("design:returntype", void 0)
+], ArticlesController.prototype, "update", null);
+__decorate([
+    (0, common_1.Delete)(':id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", void 0)
+], ArticlesController.prototype, "remove", null);
+exports.ArticlesController = ArticlesController = __decorate([
+    (0, common_1.Controller)('articles'),
+    __metadata("design:paramtypes", [articles_service_1.ArticlesService])
+], ArticlesController);
+//# sourceMappingURL=articles.controller.js.map
